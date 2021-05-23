8134b45a4bd3931be44ec15122dc6c0b30ee4ecd
Dheeraj R <83569971+DheerajR1@users.noreply.github.com>
2021-05-23 18:30:02 +0530

    Added scoring and Questions
    FEAT: Highscore listing
    STYLE: Added chalk for console stlying

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 var readLineSync = require("readline-sync");
+const chalk = require("chalk");
 
-var userName = readLineSync.question("May i know your name please? ");
-console.log("Welcome!", userName,"Let's see how well you know me!!");
+var userName = readLineSync.question(chalk.bgMagentaBright("May i know your name please?")+" ");
 
+console.log("Welcome!", chalk.underline.bold(userName) + ", Let's see how well you know me!!");
+
+var highScore = [
+  {
+    name: "Dheeraj",
+    score:5
+  },
+  {
+    name: "John Doe",
+    score:4
+  },
+  {
+    name: "John Doe 1",
+    score:2
+  }
+];
 var score = 0;
 
 var questionnaire = [
@@ -11,24 +27,64 @@ var questionnaire = [
     answer : "DextrousMonk"
   },
   {
+    question : "what's my favourite anime? ",
+    answer : "Naruto"
+  },
+  {
+    question : "where am I working from currently? ",
+    answer : "HOME"
+  },
+  {
+    question : "what's my insta handle? ",
+    answer : "ambidextrous_monk"
+  },
+  {
     question : "where do i live? ",
     answer : "bangalore"
   }
 ];
 
-function play(question, answer) {
+function play(question, answer, i) {
   var userAns = readLineSync.question(question);
 
-  if (userAns.toLowerCase() === answer) {
-    console.log("you are right");
+  var correctOrNot = userAns.toLowerCase() === answer.toLowerCase();
+
+  if (correctOrNot) {
+    console.log(chalk.green("you are right"));
     score++;
+  } else {
+    console.log(chalk.green('No! It\'s not',
+    chalk.red.underline.bold(userAns) + 
+    '. It\'s ' + chalk.yellow.bgBlackBright.underline.bold(answer)));
   }
   console.log("Your current score is:", score);
-  console.log("------------------------------");
 }
 
 for (var i = 0; i<questionnaire.length; i++) {
   var quiz = questionnaire[i];
-  play(quiz.question, quiz.answer);
+   console.log("--------------",i+1,"----------------");
+  play(quiz.question, quiz.answer, i);
 }
-console.log("You scored:", score);
+  console.log("------------------------------");
+console.log("\n\nYou scored:", score,"");
+if (score < 2) {
+  console.log( chalk.yellow.bgBlackBright.underline.bold("CALL me NOW!\n\n\n"));
+}
+console.log("Current Leaderboard");
+console.log("Name \t\t Score");
+
+var newTopper = false;
+
+for (var i = 0; i < highScore.length; i++) {
+  console.log(highScore[i].name, "\t\t", highScore[i].score);
+  if (score > highScore[i].score) {
+    newTopper = true;
+  }
+}
+if (newTopper) {
+  console.log(chalk.blue.bgGreenBright.bold("send your score, I'll add it to the Leaderboard"));
+}
+
+
+
+
